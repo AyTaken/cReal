1ef36d40b27c863ac833b21bc4f9024922688f39
RobertoAlessandri <roberto.alessandri@mail.polimi.it>
2021-02-04 10:44:48 +0100

    Ready to Work

---
+++ TonePlayer.js
@@ -0,0 +1,17 @@
+const Tone = require('tone');
+const Chord = require('./chords.js');
+console.log(Chord.getNotesChord("G7"));
+console.log("Ehi Gio");
+//create a synth and connect it to the main output (your speakers)
+const synth = new Tone.Synth().toDestination();
+
+//play a middle 'C' for the duration of an 8th note
+Tone.start()
+
+const resumeBtn = document.getElementById("resumeButton");
+resumeBtn.onclick = function() {
+    synth.triggerAttackRelease("C4", "8n");
+}
+
+console.log(Chord.getNotesChord('G7'));
+// $ browserify app.js > main.js
