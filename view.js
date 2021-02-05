9a7b6530152e0cabad14f927b24a58d11a7b0cd6
AyTaken <giovanni.affatato@mail.polimi.it>
2021-02-05 10:27:24 +0100

    Added song infos in gui

--- view.js
+++ view.js
@@ -5,22 +5,46 @@ let style
 let key
 let bpm
 let timeSignature
-let chords
+let chords = []
 let connectChords
 
+//Dom elements
+const chordPanel = document.getElementById("chords")
+const titleDiv = document.getElementById("songTitle")
+const composerDiv = document.getElementById("composer")
+const styleAndKeyDiv = document.getElementById("styleAndKey")
+
 exports.changeState = function (song) {
-    this.title = song.title
-    this.composer = song.composer
-    this.style = song.style
-    this.key = song.key
-    this.bpm = song.bpm
-    this.timeSignature = song.music.timeSignature
-    this.chords = song.music.measures
-    console.log(this.composer)
+    title = song.title
+    composer = song.composer
+    style = song.style
+    key = song.key
+    bpm = song.bpm
+    timeSignature = song.music.timeSignature
+    //Copy all measures
+    for (let i = 0; i < song.music.measures.length; i++) {
+        let temp = song.music.measures[i]
+        chords.push(temp)
+    }
+    render()
 }
 
 
 
 function render() {
+    //Render chords
+    for (let i = 0; i < chords.length; i++) {
+        let div = document.createElement("div");
+        div.id = "cell" + i
+        div.classList.add("cell")
+        div.textContent = chords[i]
+        chordPanel.appendChild(div)
+    }
+
+    //Render sidebar panel
+    titleDiv.textContent = title
+    composerDiv.textContent = composer
+    styleAndKeyDiv.textContent = style + " in " + key
+    console.log(title)
 
 }
